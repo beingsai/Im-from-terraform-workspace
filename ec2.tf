b9eb84e2f79a1c03c9b342d6d4b169df16049f09
beingsai <60880008+beingsai@users.noreply.github.com>
2026-04-10 09:25:35 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,4 +1,21 @@
-resource "aws_instance" "demo" {
-  ami           = "ami-0a914de4dc1f18727"
-  instance_type = "t2.micro"
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
   }
+}
+
+provider "aws" {
+  region = "ap-south-1" # Change if needed
+}
+
+resource "aws_instance" "demo" {
+  ami           = "ami-02a2af70a66af6dfb
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "demo-ec2"
+  }
+}
